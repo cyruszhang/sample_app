238a71abcbb50c6e3ae51cc6ee73e04c3bb1f723
yilei <yilei@amazon.com>
2012-12-09 23:59:25 -0800

    add simple user

---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password   # password_digest in DB table USERS  https://github.com/rails/rails/blob/master/activemodel/lib/active_model/secure_password.rb
+
+  validates :name, presence: true, length: {maximum: 50}
+
+  VALID_EMAIL_REGEX = /^[\w+\-\.]+@[a-z\d\-\.]+\.[a-z]+$/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
+  before_save { |user| user.email = email.downcase }
+
+  validates :password, presence: true, length: {maximum: 6}
+  validates :password_confirmation, presence: true
+
+end
