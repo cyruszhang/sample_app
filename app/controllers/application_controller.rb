70257ea5a4e779c00e348d0dde7e16d5edff82c4
yilei <yilei@amazon.com>
2012-12-10 23:20:22 -0800

    add signin and signout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper  # make the module SessionHelper available in Controllers; it is automatically available in views/session
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,8 +18,15 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /^[\w+\-\.]+@[a-z\d\-\.]+\.[a-z]+$/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } 
   before_save { self.email.downcase! }
+  before_save :create_remember_token
 
   validates :password, length: {maximum: 6}
   validates :password_confirmation, presence: true
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 
 end
