7df1fc3fd693d0b9868dc7ea7a1150709820a58f
yilei <yilei@amazon.com>
2012-12-12 21:48:30 -0800

    adding microposts - complete

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,13 +1,33 @@
 class MicropostsController < ApplicationController
   before_filter :signed_in_user, only: [:create, :destroy]
+	before_filter :correct_user, only: :destroy
 
 	def index
 	end
 
   def create
-  end
+		@micropost = current_user.microposts.build(params[:micropost])
+		if @micropost.save
+			flash[:success] = "Micropost created!"
+			redirect_to root_url
+		else
+			@feed_items = [] # error message page still needs feed_items 
+			render 'static_pages/home'
+		end
+	end
 
   def destroy
-  end
+  	@micropost.destroy
+		redirect_to root_url
+	end
+
+	
+	private
+
+		def correct_user
+			@micropost = current_user.microposts.find_by_id(params[:id])
+			redirect_to root_url if @micropost.nil?
+		end
+
 end
 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
-  end
-
-  def help
+		if signed_in?
+			@micropost = current_user.microposts.build if signed_in?
+			@feed_items = current_user.feed.paginate(page: params[:page]) if signed_in?
+  	end
+	end
+  
+	def help
   end
 
   def about
